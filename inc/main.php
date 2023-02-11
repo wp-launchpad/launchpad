1e80e8272905ea70e69af10d4bb8c8501b06cca7
cyrille <coquardcyr@gmail.com>
2023-02-11 23:50:40 +0100

    Added fixes from init script

--- inc/main.php
+++ inc/main.php
@@ -1,15 +1,16 @@
 <?php
 
+namespace PSR2Plugin;
+
 use PSR2Plugin\Dependencies\League\Container\Container;
 use PSR2Plugin\Engine\Activation\Activation;
 use PSR2Plugin\Engine\Deactivation\Deactivation;
-use PSR2Plugin\Plugin;
 
 defined( 'ABSPATH' ) || exit;
 
 // Composer autoload.
-if ( file_exists( PS2_PLUGIN_SIMULATOR_PATH . 'vendor/autoload.php' ) ) {
-    require PS2_PLUGIN_SIMULATOR_PATH . 'vendor/autoload.php';
+if ( file_exists( PS2_PLUGIN_PATH . 'vendor/autoload.php' ) ) {
+    require PS2_PLUGIN_PATH . 'vendor/autoload.php';
 }
 
 /**
@@ -17,14 +18,14 @@ if ( file_exists( PS2_PLUGIN_SIMULATOR_PATH . 'vendor/autoload.php' ) ) {
  *
  * @since 1.0
  */
-function ps2_plugin_init() {
+function plugin_init() {
     // Nothing to do if autosave.
     if ( defined( 'DOING_AUTOSAVE' ) ) {
         return;
     }
 
     // Last constants.
-    define( 'PS2_PLUGIN_PLUGIN_NAME', 'WP Rocket Lighthouse Simulator' );
+    define( 'PS2_PLUGIN_PLUGIN_NAME', 'PS2 plugin' );
     define( 'PS2_PLUGIN_PLUGIN_SLUG', sanitize_key( PS2_PLUGIN_PLUGIN_NAME ) );
 
     $wp_rocket = new Plugin(
@@ -34,7 +35,7 @@ function ps2_plugin_init() {
 
     // Call defines and functions.
 }
-add_action( 'plugins_loaded', 'ps2_plugin_init' );
+add_action( 'plugins_loaded',  __NAMESPACE__ . '\\plugin_init' );
 
 register_deactivation_hook( PS2_PLUGIN_FILE, [ Deactivation::class, 'deactivate_plugin' ] );
 register_activation_hook( PS2_PLUGIN_FILE, [ Activation::class, 'activate_plugin' ] );
